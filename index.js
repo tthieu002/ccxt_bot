091f1f392d1d8f93e14de97b9c2ab16f9811d4d5
Hiếu Trịnh <tthieu002@gmail.com>
2022-04-30 20:21:51 +0700

    1

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 const ccxt = require("ccxt");
-
 const candle = require("./core/candle");
 const constant = require("./config/constant");
 const binanceusdm = new ccxt.binanceusdm({
-        apiKey: '3b8a1940a3831a1bb88230ff25bbe0155369fbeae36b79e7a68ebb0afded6e7c',
-        secret: '330faf044231d614a03bb85bc3e1ac6b1217c3dc6d8b0e6999f9f024cca25ad0',
-    }
-);
+    apiKey: '3b8a1940a3831a1bb88230ff25bbe0155369fbeae36b79e7a68ebb0afded6e7c',
+    secret: '330faf044231d614a03bb85bc3e1ac6b1217c3dc6d8b0e6999f9f024cca25ad0',
+});
 binanceusdm.setSandboxMode(true);
 
 async function printBalance() {
@@ -24,7 +22,7 @@ async function main() {
         console.log(element);
         console.log(candle.isDojiCandle(element));
     });
-    // console.log(prices);
+
 }
 
 main();
